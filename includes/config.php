6914ac7b702dbd448eea5fb247cf2694cf351e18
Mohamed EL Bardi <elbardi065@gmail.com>
2023-03-29 23:16:46 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,9 +2,9 @@
 ob_start();
 session_start();
 define("DB_HOST","localhost");
-define("DB_USER","id19742854_ecommm");
-define("DB_PASS","m2]}\ooy)#fV[wy!");
-define("DB_NAME","id19742854_ecom");
+define("DB_USER","root");
+define("DB_PASS","");
+define("DB_NAME","students");
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 mysqli_set_charset($connection,'utf8');
 
